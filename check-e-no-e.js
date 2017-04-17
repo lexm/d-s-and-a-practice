868cb746bac65fc274889dae9b31677bd3a68ecf
Lex Myers <lex.myers@gmail.com>
2017-04-17 16:13:30 -0700

    removed unused variables

--- check-e-no-e.js
+++ check-e-no-e.js
@@ -1,8 +1,5 @@
 const assert = require('assert');
 
-var testStr1 = 'Hello, World!';
-var testStr2 = 'Hi, World!';
-
 const ltrNotH3r3 = (string) =>
   !string.split('').find((char) => ('0x' + char) * 1 === 14);
 
