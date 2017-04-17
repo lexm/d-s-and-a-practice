7dab2b7bd2f3c24f28cd611aacf5b835d3a6d8b2
Lex Myers <lex.myers@gmail.com>
2017-04-17 13:50:56 -0700

    one more angle to try

---
+++ check-e-no-e.js
@@ -0,0 +1,19 @@
+var testStr1 = 'Hello, World!';
+var testStr2 = 'Greetings, World!';
+
+var char1 = 'e';
+var char2 = 'E';
+
+var ch1 = 0 + char1;
+var ch2 = 0 + char2;
+// var ch1 = char1.charCodeAt(0);
+// var ch2 = char2.charCodeAt(0);
+
+console.log(ch1, ch2);
+// console.log(ch1 ^ 101);
+
+function ltrNotHere(string) {
+  for(var ctr0 = 0; ctr0 < string.length; ctr0++) {
+
+  }
+}
