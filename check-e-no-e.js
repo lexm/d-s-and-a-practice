165826f3fd71fe168f09cdb6f38ebb7d60fd33fb
Lex Myers <lex.myers@gmail.com>
2017-04-17 16:15:20 -0700

    let's put the fun into the function name

--- check-e-no-e.js
+++ check-e-no-e.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 
-const ltrNotH3r3 = (string) =>
+const l3tt3rNotH3r3 = (string) =>
   !string.split('').find((char) => ('0x' + char) * 1 === 14);
 
-assert(!ltrNotH3r3('Hello, World!'));
-assert(ltrNotH3r3('Hi, World!'));
-assert(!ltrNotH3r3('Big E'));
-assert(ltrNotH3r3('nothing'));
-assert(ltrNotH3r3(''));
-assert(!ltrNotH3r3('I\'m here'));
-assert(ltrNotH3r3('I\'m not'));
+assert(!l3tt3rNotH3r3('Hello, World!'));
+assert(l3tt3rNotH3r3('Hi, World!'));
+assert(!l3tt3rNotH3r3('Big E'));
+assert(l3tt3rNotH3r3('nothing'));
+assert(l3tt3rNotH3r3(''));
+assert(!l3tt3rNotH3r3('I\'m here'));
+assert(l3tt3rNotH3r3('I\'m not'));
 console.log('Woo Hoo!');
