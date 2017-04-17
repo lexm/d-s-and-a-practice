426e6b36ccb5570f8208b9b16989aa1ea9bfdb67
Lex Myers <lex.myers@gmail.com>
2017-04-17 16:11:43 -0700

    pretty much done

--- check-e-no-e.js
+++ check-e-no-e.js
@@ -3,20 +3,8 @@ const assert = require('assert');
 var testStr1 = 'Hello, World!';
 var testStr2 = 'Hi, World!';
 
-function ltrNotH3r3(string) {
-  var array = string.split('');
-  for(var dun = !!0; !dun;) {
-    var char = array.pop();
-    if(!char) {
-      dun = !0
-    }
-    // if(!dun && (('0x' + char) * 1).toString(10) === '14') {
-    if(!dun && ('0x' + char) * 1 === 14) {
-      return !!0;
-    }
-  }
-  return !0;
-}
+const ltrNotH3r3 = (string) =>
+  !string.split('').find((char) => ('0x' + char) * 1 === 14);
 
 assert(!ltrNotH3r3('Hello, World!'));
 assert(ltrNotH3r3('Hi, World!'));
